3c6f5806da6db0209141ab532c80bc5b8a81e94b
mcosnta000 <mconsta000@gmail.com>
2019-10-19 16:55:39 -0400

    implement solution

--- solution.py
+++ solution.py
@@ -1,4 +1,24 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
-        return 0.0
+import math
 
+class Solution:
+    def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:        
+        r1 = self.reduce(nums1)
+        r2 = self.reduce(nums2)
+
+        return (r1+r2) / 2
+
+    def reduce(self, nums: [int]) -> float:
+        s = len(nums)
+
+        if s == 1:
+            return nums[0]
+        elif s == 2:
+            return (nums[0] + nums[1]) / 2
+        else:
+            i = math.ceil(s / 2)
+            if s % 2 == 1:
+                return nums[i-1]
+            else:
+                return (nums[i-1:i] + nums[i:i+1]) / 2
+
+        return 0.0
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -8,7 +8,7 @@ class SolutionTestCase(unittest.TestCase):
         self.solution = Solution()
 
     def runAssertion(self, a, b):
-        self.assertEqual(median(a+b), self.solution.findMedianSortedArrays(a,b))
+        self.assertEqual((median(a) + median(b)) / 2 , self.solution.findMedianSortedArrays(a,b))
 
     def test1(self):
         a = [1,3]
@@ -18,7 +18,13 @@ class SolutionTestCase(unittest.TestCase):
 
     def test2(self):
         a = [1,2]
-        b = [3, 4]
+        b = [3,4]
+
+        self.runAssertion(a,b)
+
+    def test3(self):
+        a = [2,4]
+        b = [1,2,3,4,5,6,7,10,100]
 
         self.runAssertion(a,b)
         
