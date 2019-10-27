4a0546ec318d9fe3f9e018990c5408b31be63860
mcosnta000 <mconsta000@gmail.com>
2019-10-27 10:46:03 -0400

    Working Impl

--- solution.py
+++ solution.py
@@ -1,24 +1,33 @@
 import math
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:        
-        r1 = self.reduce(nums1)
-        r2 = self.reduce(nums2)
+    def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
+        nums = self.merge(nums1, nums2)
+        nums_len = len(nums)
+        mid_point = nums_len // 2
 
-        return (r1+r2) / 2
-
-    def reduce(self, nums: [int]) -> float:
-        s = len(nums)
-
-        if s == 1:
-            return nums[0]
-        elif s == 2:
-            return (nums[0] + nums[1]) / 2
+        if nums_len % 2 == 0:
+            return (nums[mid_point-1] + nums[mid_point]) / 2
         else:
-            i = math.ceil(s / 2)
-            if s % 2 == 1:
-                return nums[i-1]
-            else:
-                return (nums[i-1:i] + nums[i:i+1]) / 2
+            return nums[mid_point]
 
-        return 0.0
+    def merge(self, nums1: [int], nums2: [int]) -> [int]:
+        nums1_len = len(nums1)
+        nums2_len = len(nums2)
+
+        if nums1_len == 0 or nums2_len == 0:
+            return nums1 + nums2
+        elif nums1[nums1_len-1] <= nums2[0]:
+            return nums1 + nums2
+        elif nums2[nums2_len-1] <= nums1[0]:
+            return nums2 + nums1
+        else:
+            split1 = self.split(nums1)
+            split2 = self.split(nums2)
+            
+            return self.merge(split1[0], split2[0]) + self.merge(split1[1], split2[1])
+
+    def split(self, nums: [int]) -> [[int]]:
+        mid_point = len(nums) // 2
+
+        return [nums[:mid_point], nums[mid_point:]]
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -10,37 +10,67 @@ class SolutionTestCase(unittest.TestCase):
     def runAssertion(self, a, b):
         self.assertEqual((median(a + b)) , self.solution.findMedianSortedArrays(a,b))
 
-    def test1(self):
+    def test_split1(self):
+        self.assertEqual(self.solution.split([1,2,3]), [[1],[2,3]])
+
+    def test_split2(self):
+        self.assertEqual(self.solution.split([1,2,3,4]), [[1,2],[3,4]])
+
+    def test_split2(self):
+        self.assertEqual(self.solution.split([1]), [[],[1]])
+
+    def test_merge1(self):
+        self.assertEqual(self.solution.merge([1], []), [1])
+
+    def test_merge2(self):
+        self.assertEqual(self.solution.merge([], [1]), [1])
+
+    def test_merge3(self):
+        self.assertEqual(self.solution.merge([1], [2]), [1,2])
+
+    def test_merge4(self):
+        self.assertEqual(self.solution.merge([2], [1]), [1,2])
+
+    def test_merge6(self):
+        self.assertEqual(self.solution.merge([1,3], [2]), [1,2,3])
+
+    def test_merge7(self):
+        self.assertEqual(self.solution.merge([3], [2,4]), [2,3,4])
+
+    def test_merge8(self):
+        self.assertEqual(self.solution.merge([2,2], [2,3]), [2,2,2,3])
+
+    def test_merge7(self):
+        self.assertEqual(self.solution.merge([2], [1,3]), [1,2,3])
+
+    def test_merge8(self):
+        self.assertEqual(self.solution.merge([1,4], [2,3]), [1,2,3,4])
+
+    def test_median1(self):
         a = [1,3]
         b = [2]
 
         self.runAssertion(a,b)
 
-    def test2(self):
+    def test_median2(self):
         a = [1,2]
         b = [3,4]
 
         self.runAssertion(a,b)
 
-    def test3(self):
+    def test_median3(self):
         a = [2,4]
         b = [1,2,3,4,5,6,7,10,100]
 
         self.runAssertion(a,b)
 
-    def test4(self):
+    def test_median4(self):
         a = [2,2,2,2]
         b = [4,4,4,4]
 
         self.runAssertion(a,b)
 
-    def test4(self):
-        a = [2,2,2,2]
-        b = [4,4,4,4]
-
-        self.runAssertion(a,b)
-
-    def test5(self):
+    def test_median5(self):
         i = 0
         a = []
         b = []
