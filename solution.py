155c088d5255a4b2bdabd53ffdc47d62834099eb
mcosnta000 <mconsta000@gmail.com>
2019-11-03 09:31:40 -0500

    Change approach to use binary search

--- solution.py
+++ solution.py
@@ -1,29 +1,40 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
-        nums = self.merge(nums1, nums2)
-        nums_len = len(nums)
-        mid_point = nums_len // 2
+        return 0.0
 
-        if nums_len % 2 == 0:
-            return (nums[mid_point-1] + nums[mid_point]) / 2
+    def search(self, nums: [int], low: int, high: int, target: int) -> int:
+        mid = (low + high) // 2
+
+        if (low > high):
+            return low
+        elif target == nums[mid]:
+            return mid
+        elif target > nums[mid]:
+            return self.search(nums, mid+1, high, target)
         else:
-            return nums[mid_point]
+            return self.search(nums, low, mid-1, target)
 
-    def merge(self, nums1: [int], nums2: [int]) -> [int]:
+    def merge(self, nums1: [int], nums2: [int]):
         nums1_len = len(nums1)
         nums2_len = len(nums2)
 
-        if nums1_len == 0 or nums2_len == 0:
-            return nums1 + nums2
-        elif nums1[nums1_len-1] <= nums2[0]:
-            return nums1 + nums2
-        elif nums2[nums2_len-1] <= nums1[0]:
-            return nums2 + nums1
-        else:
-            split1 = self.split(nums1)
-            split2 = self.split(nums2)
+        median_index = (nums1_len + nums2_len) // 2
+
+        i = t = 0
+        while (i < nums2_len and t < nums1_len):
+            t = self.search(nums1, t, nums1_len-1, nums2[i])
             
-            return self.merge(split1[0], split2[0]) + self.merge(split1[1], split2[1])
+            if (t > nums1_len-1):
+                return
+            elif nums1[t] > nums2[i]:
+                self.swap(nums1, t, nums2, i)
+                t = t + 1
+            else:
+                i = i + 1
+
+    def swap(self, nums1: [int], i1: int, nums2: [int], i2: int):
+        nums1[i1],nums2[i2] = nums2[i2],nums1[i1]
+
 
     def split(self, nums: [int]) -> [[int]]:
         mid_point = len(nums) // 2
